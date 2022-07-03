f785276151e0609d1bb0e556a04e81d45dd4b957
Paul Eggerling-Boeck <paulboeck@gmail.com>
2022-07-03 11:14:03 -0500

    added user pool config

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-2_eUTbhYVPJ', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2icat7mr3avbj5fgtntu39604d', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
