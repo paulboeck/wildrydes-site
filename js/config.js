ca106fa7b2abda6c27ac557730363d05a4ea8fe5
Paul Eggerling-Boeck <paulboeck@gmail.com>
2022-07-03 13:19:51 -0500

    added api url

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'us-east-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://qjuv3hljs0.execute-api.us-east-2.amazonaws.com/Prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
